9fa8744b053014b5d02ce35fe68a55aa16cd2e4b
Shantnu Agarwal <shantnu.agarwal@gmail.com>
2019-07-28 19:32:27 +0530

    Add project files.

---
+++ ConsoleAppCore/Login.cs
@@ -0,0 +1,75 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.IO;
+
+namespace ConsoleAppCore
+{
+	class Login
+	{
+		private string password;
+		private const string defpass = "shantnu";
+		
+		public Login()
+		{
+			/*Console.WriteLine("New Login Object Created.");*/
+			FileStream fs = new FileStream("credentials.dat", FileMode.Open);
+			BinaryReader br = new BinaryReader(fs);
+			password = br.ReadString();
+			br.Close();
+			fs.Close();
+		}
+
+		public bool check(string password)
+		{
+			if (string.Compare(password,this.password)==0)
+				return true;
+			else
+				return false;
+
+		}
+
+		public bool reset()
+		{
+			try
+			{
+				FileStream fs = new FileStream("credentials.dat", FileMode.OpenOrCreate, FileAccess.Write);
+				BinaryWriter bw = new BinaryWriter(fs);
+				bw.Write(defpass);
+				bw.Close();
+				fs.Close();
+				return true;
+			}
+
+			catch (Exception e)
+			{
+				Console.WriteLine("There was an error reseting the password. Error Info: " + e);
+				return false;
+			} 
+		}
+
+		public static void testsql()
+		{
+			MySqlConnection con = new MySqlConnection("server=127.0.0.1;user id=root;database=schema1;password=welcome");
+			con.Open();
+			MySqlCommand cmd = new MySqlCommand("select * from abc",con);
+			MySqlDataAdapter adptr = new MySqlDataAdapter(cmd);
+			DataSet ds = new DataSet();
+			adptr.Fill(ds,"abc");
+			DataTable dt = ds.Tables["abc"];
+			foreach (DataRow row in dt.Rows)
+			{
+				foreach (DataColumn col in dt.Columns)
+				{
+					Console.Write(row[col] + "\t");
+				}
+
+				Console.WriteLine();
+			}
+			cmd.Dispose();
+			con.Close();
+			Console.ReadKey();
+		}
+
+	}
+}
---
+++ ConsoleAppCore/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ConsoleAppCore
+{
+	class Program
+	{
+		const double pi = Math.PI;
+		static int logincnt=0;
+
+		private static void startLogin()
+		{
+			Console.Write("What's your password? : ");
+			String password = "";
+			logincnt++;
+			
+			char inx = Convert.ToChar(Console.Read());
+			while (inx >= 'a' && inx <='z')
+			{
+				password += inx;
+				inx = Convert.ToChar(Console.Read());
+			}
+			Login login = new Login();
+			if (!login.check(password))
+			{
+				Console.Clear();
+				if (logincnt == 6) {
+					if(login.reset())
+						Console.WriteLine("Too many bad attempts. The password has been reset. Bye.");
+					Console.ReadKey();
+				}
+			
+				else
+				{
+					Console.WriteLine("Nah, this is not the correct password.");
+					startLogin();
+				}
+				
+			}
+			else
+			{
+				Console.WriteLine("You have successfully unlocked the secrets of C#!\nPress any key to continue.\n");
+				Console.ReadKey();
+				proceed();
+			}
+		}
+	
+		private static void proceed()
+		{
+
+			int max = int.MaxValue;
+			int min = int.MinValue;
+
+			Console.WriteLine($"Max allowed int is : {max}");
+			Console.WriteLine($"Min allowed int is : {min}");
+			Console.WriteLine($"Pi value: {pi}");
+
+			decimal min2 = decimal.MinValue;
+			decimal max2 = decimal.MaxValue;
+			Console.WriteLine($"The range of the decimal type is {min2} to {max2}");
+			Console.ReadKey();
+		}
+		static void Main(string[] args)
+		{
+			startLogin();
+			Login.testsql();
+			
+			
+				
+			
+
+
+		}
+	}
+
+	
+
+}
